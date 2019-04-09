e2045534b1c0526071aba4ee4b6c281142f08989
regnveig <regnveig@yandex.ru>
2019-04-09 22:26:13 +0700

    Lazy commit

--- Endless/weather.h
+++ Endless/weather.h
@@ -3,6 +3,43 @@
 
 #include "global.h"
 
+
+
+struct weather_data {
+
+    enum class weather_type {
+
+        Clear,
+        Fog,
+        Rain,
+        Storm,
+        Cloudy,
+        Snow,
+        Blizzard
+    };
+
+    weather_type    type;
+    qreal           wind_angle,
+                    wind_strength;
+    qreal           weather_angle,
+                    weather_strength_start,
+                    weather_strength_end;       // Для WeatherBox'а
+
+};
+
+class Cyclone {
+public:
+
+                    Cyclone();
+                    ~Cyclone();
+
+private:
+
+    qint8           strength;       // + for cyclone, - for anticyclone
+    QVector3D       eye_coord;
+    quint16         life_time;      // sec?
+};
+
 class Weather : public QObject {
 
     Q_OBJECT
