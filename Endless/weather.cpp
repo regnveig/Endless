4b3db468b8ffaa430b07f306c25ca4459befaa49
regnveig <regnveig@yandex.ru>
2019-04-18 23:42:27 +0700

    Lazy commit

--- Endless/weather.cpp
+++ Endless/weather.cpp
@@ -1,60 +1,21 @@
 #include "weather.h"
 
-/* ------ CYCLONE SUBCLASS ------ */
-
-Cyclone::Cyclone(qint8 new_strength, QVector3D new_eye_coord, quint32 new_life_time) {
-
-    *strength = new_strength;
-    *eye_coord = new_eye_coord;
-    *life_time = new_life_time;
-}
-
-Cyclone::~Cyclone() {
-
-    delete strength; strength = nullptr;
-    delete eye_coord; eye_coord = nullptr;
-    delete life_time; life_time = nullptr;
-}
-
-bool Cyclone::Tick(QVector3D summar_power) {
-
-    *eye_coord += summar_power;
-    eye_coord->normalize();
-
-    (*life_time)--;
-    return (*life_time <= 0);
-}
-
-qint8 Cyclone::getStrength() { return *strength; }
-
-QVector3D Cyclone::getEyeCoord() { return *eye_coord; }
-
 /* ------ WEATHER CLASS ------ */
 
 Weather::Weather(QObject *parent) : QObject(parent) {
 
-    Play(true);
 }
 
 Weather::~Weather() {
 
-    Play(false);
 }
 
 void Weather::Play(bool play) {
 
-    if (play) *Timer_ID = startTimer(100);
-    else killTimer(*Timer_ID);
 }
 
-void Weather::SkyData(QList<celestial_data> data) {
 
-    for (auto item = 0; item < data.size(); item++)
-        if (data.at(item).name == QString("sun"))
-            *Z = data.at(item).vect1.z();
-}
-
-void Weather::timerEvent([[maybe_unused]] QTimerEvent *event) {
+void Weather::timerEvent(QTimerEvent *event) {
 
 
 }
--- Endless/weather.h
+++ Endless/weather.h
@@ -3,44 +3,6 @@
 
 #include "global.h"
 
-struct weather_data {
-
-    enum class weather_type {
-
-        Clear,
-        Fog,
-        Rain,
-        Storm,
-        Cloudy,
-        Snow,
-        Blizzard
-    };
-
-    weather_type    type;
-    qreal           wind_angle,
-                    wind_strength;
-    qreal           weather_angle,
-                    weather_strength_start,
-                    weather_strength_end;       // Для WeatherBox'а
-
-};
-
-class Cyclone {
-public:
-
-                    Cyclone         (qint8 new_strength, QVector3D new_eye_coord, quint32 new_life_time);
-                    ~Cyclone();
-    bool            Tick            (QVector3D summar_power);
-    qint8           getStrength     ();
-    QVector3D       getEyeCoord     ();
-
-private:
-
-    qint8           *strength       = new qint8();       // + for cyclone, - for anticyclone
-    QVector3D       *eye_coord      = new QVector3D();
-    quint32         *life_time      = new quint32();      // sec?
-};
-
 class Weather : public QObject {
 
     Q_OBJECT
@@ -57,7 +19,6 @@ signals:
 public slots:
 
     void            Play            (bool play);
-    void            SkyData         (QList<celestial_data> data);
 
 protected:
 
@@ -65,9 +26,6 @@ protected:
 
 private:
 
-    float           *Z              = new float();
-    int             *Timer_ID       = new int();
-    QList<Cyclone>  *cyclones       = new QList<Cyclone>();
 };
 
 #endif // WEATHER_H
